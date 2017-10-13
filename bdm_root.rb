69f374885bffcaedf6ee107b64981f4bac0bc9f7
Ahmad Hesam <hesam212@gmail.com>
2017-10-13 15:02:18 +0200

    Update bdm_root.rb

--- bdm_root.rb
+++ bdm_root.rb
@@ -3,7 +3,8 @@ class BdmRoot < Formula
   homepage "https://github.com/root-project/root"
   url "https://cernbox.cern.ch/index.php/s/K3ODazpS4PUjrbQ/download"
   sha256 "879f714dd9d41b2ce061019d3baa2d5ddc378ed486cd1cd0bc75841e6fede549"
-
+  version "6.10.11"
+  
   bottle :unneeded
 
   def install
