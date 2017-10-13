0bcebf2bf723ca018648673181e6862c024ca754
Ahmad Hesam <hesam212@gmail.com>
2017-10-13 14:31:36 +0200

    Create bdm_root.rb

---
+++ bdm_root.rb
@@ -0,0 +1,16 @@
+class ROOT < Formula
+  desc "Data Analysis Framwork"
+  homepage "https://github.com/root-project/root"
+  url "https://cernbox.cern.ch/index.php/s/K3ODazpS4PUjrbQ/download"
+  sha256 "879f714dd9d41b2ce061019d3baa2d5ddc378ed486cd1cd0bc75841e6fede549"
+
+  bottle :unneeded
+
+  def install
+    system "echo TODO: COPY ROOT TO CORRECT DIRECTORY"
+  end
+
+  test do
+    system "false"
+  end
+end
