60c02af68b3bcf8a00fe8cd4359082f81ccff173
Ahmad Hesam <hesam212@gmail.com>
2017-10-13 11:39:15 +0200

    Update BioDynaMo.rb

--- BioDynaMo.rb
+++ BioDynaMo.rb
@@ -1,7 +1,7 @@
 class Biodynamo < Formula
   desc "Biological Dynamic Modeler"
   homepage "https://github.com/Senui/biodynamo"
-  url "https://cernbox.cern.ch/remote.php/webdav/test/biodynamo-v1.0.0.tar.gz"
+  url "https://cernbox.cern.ch/index.php/s/KBdHNIYVTDzjNNn/download?path=%2F&files=biodynamo-v1.0.0.tar.gz"
   sha256 "879f714dd9d41b2ce061019d3baa2d5ddc378ed486cd1cd0bc75841e6fede549"
 
   bottle :unneeded
