91cd99d5b4f3c7984301ac97ecae57ba29a95af3
Ahmad Hesam <hesam212@gmail.com>
2017-10-13 15:03:00 +0200

    Update bdm_paraview.rb

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -3,7 +3,8 @@ class BdmParaview < Formula
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/tUOz6ctoEFU3cdB/download"
   sha256 "879f714dd9d41b2ce061019d3baa2d5ddc378ed486cd1cd0bc75841e6fede549"
-
+  version "5.4.1"
+  
   bottle :unneeded
 
   def install
