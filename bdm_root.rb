0677f5bf9dba56467b21644194d9a2d8208e93d5
Ahmad Hesam <hesam212@gmail.com>
2017-10-16 11:18:15 +0200

    Update bdm_root.rb

--- bdm_root.rb
+++ bdm_root.rb
@@ -1,7 +1,7 @@
 class BdmRoot < Formula
   desc "Data Analysis Framwork"
   homepage "https://github.com/root-project/root"
-  url "https://cernbox.cern.ch/index.php/s/K3ODazpS4PUjrbQ/download"
+  url "https://cernbox.cern.ch/index.php/s/ZW43ZpIIZBPI7xe/download"
   sha256 "879f714dd9d41b2ce061019d3baa2d5ddc378ed486cd1cd0bc75841e6fede549"
   version "6.10.11"
   
