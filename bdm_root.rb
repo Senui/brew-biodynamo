14f6749072c96f5cebb23f28febafd03d0d74412
Ahmad Hesam <hesam212@gmail.com>
2017-10-16 11:55:37 +0200

    Update bdm_root.rb

--- bdm_root.rb
+++ bdm_root.rb
@@ -2,7 +2,7 @@ class BdmRoot < Formula
   desc "Data Analysis Framwork"
   homepage "https://github.com/root-project/root"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=root_v6.11.01_macos64_LLVM-Clang-4.0_263508429d.tar.gz"
-  sha256 "8b2b15ede7a25e86f3cca19cba3193a96492b8564111a28db4a0e7ee3fb3a493"
+  sha256 "fe0143db4b7a0abc4265d5512efb66c96477bcadda87e6c450140b7da8e84ea8"
   version "6.11.01"
   
   bottle :unneeded
