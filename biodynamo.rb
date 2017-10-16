7f2a5f63ce0593fc9b2ca770e909e338538df6b2
Ahmad Hesam <hesam212@gmail.com>
2017-10-16 11:04:29 +0200

    Update biodynamo.rb

--- biodynamo.rb
+++ biodynamo.rb
@@ -2,24 +2,25 @@ class Biodynamo < Formula
   desc "Biological Dynamic Modeler"
   homepage "https://github.com/Senui/biodynamo"
   # link to source tarball
-  url "https://github.com/Senui/biodynamo/archive/v0.0.9.tar.gz"
+  url "https://github.com/Senui/biodynamo/releases/download/v0.0.9/biodynamo-0.0.9.sierra.bottle.tar.gz"
   # shasum of source tarball
-  sha256 "f057d60d1970d2e8e3973d77d5b7fd8ef1d9ae93d68b7d57a8f9df30e992bbbc"
+  sha256 "0681592fa16a7abd89416dca35e7721ccf59d70f45b2df13c80f8a1e17af20ef"
 
-  bottle do
-    # url to pre-built binary (a.k.a. "keg")
-    root_url "https://github.com/Senui/biodynamo/releases/download/v0.0.9"
-    # shasum of keg with reference to OS codename
-    sha256 "0681592fa16a7abd89416dca35e7721ccf59d70f45b2df13c80f8a1e17af20ef" => :sierra
-  end
+#   bottle do
+#     # url to pre-built binary (a.k.a. "keg")
+#     root_url "https://github.com/Senui/biodynamo/releases/download/v0.0.9"
+#     # shasum of keg with reference to OS codename
+#     sha256 "0681592fa16a7abd89416dca35e7721ccf59d70f45b2df13c80f8a1e17af20ef" => :sierra
+#   end
   
   depends_on "Senui/biodynamo/bdm_root" => :build
   #depends_on "Senui/biodynamo/bdm_paraview" => :recommended
 
   # if the bottle for some reason fails, we build from source
   def install
-    system "cmake ."
-    system "make", "install"
+    prefix.install Dir["biodynamo/0.0.9/*]
+#     system "cmake ."
+#     system "make", "install"
   end
 
   test do
