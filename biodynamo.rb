2fe4295644581e9dbe3acaa76128ba230c6b1f9e
Ahmad Hesam <hesam212@gmail.com>
2017-10-17 16:30:32 +0200

    Update biodynamo.rb

--- biodynamo.rb
+++ biodynamo.rb
@@ -5,6 +5,8 @@ class Biodynamo < Formula
   sha256 "0681592fa16a7abd89416dca35e7721ccf59d70f45b2df13c80f8a1e17af20ef"
 
   depends_on "Senui/biodynamo/bdm_root" => :build
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build
   depends_on "Senui/biodynamo/bdm_paraview" => :recommended
 
   def install
