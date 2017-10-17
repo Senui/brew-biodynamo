91e1c777369c302a2290cbd2b3ed38304a6ac1fb
Ahmad Hesam <hesam212@gmail.com>
2017-10-17 19:05:47 +0200

    Update biodynamo.rb

--- biodynamo.rb
+++ biodynamo.rb
@@ -2,7 +2,7 @@ class Biodynamo < Formula
   desc "Biological Dynamic Modeler"
   homepage "https://github.com/Senui/biodynamo"
   url "https://github.com/Senui/biodynamo/releases/download/v0.0.9/biodynamo-0.1.0.sierra.tar.gz"
-  sha256 "94a764b694caa0409d84af4b410c426c83b778716c548a735b03e7266bcc4934"
+  sha256 "87c15599f594e25e093d8b58dabf7396805311aebdd37342fbab79be5a38237f"
 
   depends_on "Senui/biodynamo/bdm_root" => :build
   depends_on "cmake" => :build
